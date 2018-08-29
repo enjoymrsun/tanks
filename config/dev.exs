d68d21575bc74ea94d440a6e3908608d76773bb8
enjoymrsun <derek_x_sun@163.com>
2018-08-29 17:08:09 +0800

    redesign home page

--- config/dev.exs
+++ config/dev.exs
@@ -51,8 +51,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :tanks, Tanks.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "tanksdb",
-  password: "123tanks902",
+  username: "derekDS",
+  password: "sxs07@WoaiDS",
   database: "tanks_dev",
   hostname: "localhost",
   pool_size: 10
