88a40aee2106b9ddb7e28fbbbd1aa13c8b648e0e
enjoymrsun <derek_x_sun@163.com>
2018-09-01 07:06:26 +0800

    redesign config

--- config/config.exs
+++ config/config.exs
@@ -4,7 +4,6 @@
 # This configuration file is loaded before any dependency and
 # is restricted to this project.
 use Mix.Config
-use Mix.Config.Agent
 
 # General application configuration
 config :tanks,
